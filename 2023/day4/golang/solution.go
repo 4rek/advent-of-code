8670b70602861c6873dc0814350c3ac258b8e517
Arkadiusz Juszczyk <arkadiuszjuszczyk@hotmail.com>
2023-12-04 20:23:43 +0100

    feat. more optimal solution for day4

--- 2023/day4/golang/solution.go
+++ 2023/day4/golang/solution.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 	"strings"
 	"time"
 
@@ -38,48 +39,61 @@ func partOne(lines []string) int {
 	return result
 }
 
+type Cart struct {
+	Wins  int
+	Count int
+}
+
 func partTwo(lines []string) int {
 	startTime := time.Now()
-	d := make(map[int]int, len(lines))
+	d := make(map[int]Cart, len(lines))
 
 	for i := range lines {
-		d[i] = 0
+		d[i] = Cart{0, 1}
 	}
 
 	for lineIdx, line := range lines {
-		for i := 0; i <= d[lineIdx]; i++ {
-			local := 0
+		local := 0
 
-			numbers := strings.Split(strings.Split(line, ":")[1], "|")
+		numbers := strings.Split(strings.Split(line, ":")[1], "|")
 
-			winning := slices.DeleteFunc(strings.Split(numbers[0], " "), func(e string) bool {
-				return e == ""
-			})
-			having := slices.DeleteFunc(strings.Split(numbers[1], " "), func(e string) bool {
-				return e == ""
-			})
+		winning := slices.DeleteFunc(strings.Split(numbers[0], " "), func(e string) bool {
+			return e == ""
+		})
+		having := slices.DeleteFunc(strings.Split(numbers[1], " "), func(e string) bool {
+			return e == ""
+		})
 
-			for _, h := range having {
-				if slices.Contains(winning, h) {
-					local += 1
-				}
-			}
-
-			if local > 0 {
-				for j := lineIdx + 1; j < lineIdx+1+local; j++ {
-					d[j] += 1
-				}
+		for _, h := range having {
+			if slices.Contains(winning, h) {
+				local += 1
 			}
 		}
+
+		if local > 0 {
+			d[lineIdx] = Cart{local, 1}
+		}
+
 	}
 
 	sum := 0
 
-	for _, v := range d {
-		sum += v
+	keys := make([]int, 0, len(d))
+	for k := range d {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys) // Sort the keys
+
+	for _, k := range keys {
+		for i := k + 1; i < k+1+d[k].Wins; i++ {
+			c := d[i]
+			d[i] = Cart{c.Wins, c.Count + 1*d[k].Count}
+		}
+		sum += d[k].Count
 	}
 
 	duration := time.Since(startTime)
 	fmt.Println("time taken: ", duration)
-	return sum + len(lines)
+	return sum
 }
--- 2023/day4/python/solution.py
+++ 2023/day4/python/solution.py
@@ -20,27 +20,33 @@ def part1(lines: List[str]):
     return result
 
 def part2(lines: List[str]):
-    d = { i: 0 for i in range(0, len(lines))}
+    d = {}
+
+    for i in range(0, len(lines)):
+        d[i] = {"wins": 0, "count": 1}
 
     for line_idx, line in enumerate(lines):        
-        for _ in range(0, d[line_idx] + 1):
-            local_result = 0
-            numbers = line.strip().split(":")[1].strip().split('|')
-            winning = list(filter(len, numbers[0].strip().split(" ")))
-            having = list(filter(len, numbers[1].strip().split(" ")))
+        local_result = 0
+        numbers = line.strip().split(":")[1].strip().split('|')
+        winning = list(filter(len, numbers[0].strip().split(" ")))
+        having = list(filter(len, numbers[1].strip().split(" ")))
 
-            for h in having:
-                if h in winning:
-                    local_result += 1
+        for h in having:
+            if h in winning:
+                local_result += 1
 
-            if local_result > 0:
-                for x in range(line_idx+1, line_idx + 1 + local_result):
-                    d[x] += 1
+        d[line_idx]["wins"] = local_result
+    
+    s = 0
 
-    return sum(d.values()) + len(lines)
+    for key, value in d.items():
+        for x in range(key+1, key + 1 + value["wins"]):
+            d[x]["count"] += 1 * value["count"]
+        s += value["count"]
 
+    return s
 
 if __name__ == "__main__":
     f = open(os.getcwd() + '/day4/inputs/main.txt', 'r')
     lines = f.readlines()
-    part2(lines)
+    print(part2(lines))
