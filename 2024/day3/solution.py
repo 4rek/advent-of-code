1fa0775e44b4b1aa7251f95f975a362768892d5d
A. Juszczyk <arkadiusz@tonik.com>
2024-12-03 07:34:41 +0100

    feat. 2024/03 solutions

---
+++ 2024/day3/solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+import os
+import re
+
+def calculate(s: str) -> int:
+    matches = re.findall(r'mul\(([0-9]{1,3}),([0-9]{1,3})\)', s)
+    result = [int(x)*int(y) for x, y in matches]
+    return sum(result)
+
+
+def part1(lines: List[str]):
+    r = "".join(lines)
+    return calculate(r)
+
+def part2(lines: List[str]):
+    r = "".join(lines).split("do()")
+    z = ""
+    
+    for i in r:
+        z += i.split("don't()")[0]
+
+    return calculate(z)
+
+
+if __name__ == "__main__":
+    f = open(f"{os.getcwd()}/day3/inputs/main.txt", "r")
+    lines = f.readlines()
+    print("Part 1: ", part1(lines))
+    print("Part 2: ", part2(lines))
---
+++ 2024/day3/test_day3.py
@@ -0,0 +1,31 @@
+from .solution import part1, part2
+import os
+
+day = "3"
+
+def test_part1_demo():
+    f = open(f"{os.getcwd()}/day{day}/inputs/demo1.txt", "r")
+    lines = f.readlines()
+
+    assert part1(lines) == 2
+
+
+def test_part1():
+    f = open(f"{os.getcwd()}/day{day}/inputs/main.txt", "r")
+    lines = f.readlines()
+
+    assert part1(lines) == 213
+
+
+def test_part2_demo():
+    f = open(f"{os.getcwd()}/day{day}/inputs/demo2.txt", "r")
+    lines = f.readlines()
+
+    assert part2(lines) == 4
+
+
+def test_part2():
+    f = open(f"{os.getcwd()}/day{day}/inputs/main.txt", "r")
+    lines = f.readlines()
+
+    assert part2(lines) == 285
