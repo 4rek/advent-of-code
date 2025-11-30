1451456c6458c90a3e7322a854416ac9b72591ca
A. Juszczyk <arkadiusz@tonik.com>
2025-11-30 21:23:48 +0100

    feat: ready for 2025

---
+++ 2025/template/solution.ts
@@ -0,0 +1,7 @@
+export function partOne(data: string[]): number {
+  return 1
+}
+
+export function partTwo(data: string[]): number {
+  return 2
+}
---
+++ 2025/template/test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from '@jest/globals';
+import { partOne, partTwo } from './solution';
+
+import { readFile } from 'fs/promises';
+
+describe('Day 1', () => {
+
+  test('Part one - test data', async () => {
+    const input: string = await readFile(__dirname + '/test-input.txt', 'utf8');
+    const data = input.split('\n\n');
+
+    expect(partOne(data)).toBe(1);
+  });
+
+  test('Part one - main data', async () => {
+    const input: string = await readFile(__dirname + '/input.txt', 'utf8');
+    const data = input.split('\n\n');
+
+    expect(partOne(data)).toBe(1);
+  })
+
+  test('Part two - test data', async () => {
+    const input: string = await readFile(__dirname + '/test-input.txt', 'utf8');
+    const data = input.split('\n\n');
+
+    expect(partTwo(data)).toBe(2);
+  })
+
+  test('Part two - main data', async () => {
+    const input: string = await readFile(__dirname + '/input.txt', 'utf8');
+    const data = input.split('\n\n');
+
+    expect(partTwo(data)).toBe(2);
+  })
+});
